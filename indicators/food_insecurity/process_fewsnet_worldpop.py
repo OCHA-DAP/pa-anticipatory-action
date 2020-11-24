c68a292596bb5778bc078bd909fa36ff3fd452f7
Josee Poirier <josee.phd@gmail.com>
2020-11-24 15:59:29 -0500

    utils copied from old repo

---
+++ indicators/food_insecurity/process_fewsnet_worldpop.py
@@ -0,0 +1,257 @@
+import pandas as pd
+import os
+import geopandas as gpd
+from rasterstats import zonal_stats
+import numpy as np
+from utils import parse_args, parse_yaml, config_logger
+from pathlib import Path
+import logging
+from tqdm import tqdm
+
+logger = logging.getLogger(__name__)
+
+
+def merge_fewsnet_population(fews_path, adm_path, pop_path, date, period, adm1c, adm2c):
+    """
+    Compute the population per IPC phase per adm2 region for the data defined in fews_path
+    Args:
+        fews_path: path to the shapefile with FewsNet data
+        adm_path: path to the shapefile with admin2 boundaries
+        pop_path: path to the raster file with population data
+        date: date of the data defined in fews_path
+        period: type of FewsNet prediction: CS (current), ML1 (near-term projection) or ML2 (medium-term projection)
+        adm1c: column name of the admin1 level name, in adm_path data
+        adm2c: column name of the admin2 level name, in adm_path data
+
+    Returns:
+        df_gp: DataFrame with the population per IPC phase per Admin2
+    """
+    df_fews = gpd.read_file(fews_path)
+    df_adm = gpd.read_file(adm_path)
+    # get fewsnet area (livelihood) per admin region in df_adm (generally admin2)
+    # overlay takes really long to compute, but could not find a better method
+    df_fewsadm = gpd.overlay(df_adm, df_fews, how="intersection")
+
+    # calculate population per "geometry"
+    # in pop_path, the value per cell is the population of that cell, so we want the sum of them
+    # in the calculation a cell is considered to belong to an area if the center of that cell is inside the area.
+    # see https://pythonhosted.org/rasterstats/manual.html#rasterization-strategy
+    df_fewsadm["pop"] = pd.DataFrame(
+        zonal_stats(vectors=df_fewsadm["geometry"], raster=pop_path, stats="sum")
+    )["sum"]
+
+    # convert the period values (1 to 5) to str
+    df_fewsadm[period] = df_fewsadm[period].astype(int).astype(str)
+    df_g = df_fewsadm.groupby([adm1c, adm2c, period], as_index=False).sum()
+    # set the values of period as columns (1,2,3,4,5,99)
+    df_gp = df_g.pivot(index=[adm1c, adm2c], columns=period, values="pop")
+    df_gp = df_gp.add_prefix(f"{period}_")
+    df_gp.columns.name = None
+    # not all IPC levels will be present in all admin regions, so fill those with zeroes
+    df_gp = df_gp.replace(np.nan, 0)
+    df_gp = df_gp.reset_index()
+    df_gp["date"] = pd.to_datetime(date, format="%Y%m")
+    return df_gp
+
+
+def combine_fewsnet_projections(
+    country_iso3,
+    dates,
+    folder_fews,
+    folder_pop,
+    admin_path,
+    shp_adm1c,
+    shp_adm2c,
+    region,
+    regionabb,
+    country_iso2,
+    result_folder,
+    start_date,
+    end_date,
+):
+    """
+    Retrieve all FewsNet data, and calculate the population per IPC phase per date-admin combination
+    The results are saved to a csv, one containing the admin2 calculations and one the admin1.
+    Args:
+        country_iso3: string with iso3 code
+        dates: list of dates for which FewsNet data should be included
+        folder_fews: path to folder that contains the FewsNet data
+        folder_pop: path to folder that contains the population data
+        admin_path: path to the shapefile with admin2 boundaries
+        shp_adm1c: column name of the admin1 level name, in adm_path data
+        shp_adm2c: column name of the admin2 level name, in adm_path data
+        region: region that the fewsnet data covers, e.g. "east-africa"
+        regionabb: abbreviation of the region that the fewsnet data covers, e.g. "EA"
+        iso2_code: iso2 code of the country of interest
+        result_folder: path to folder to which to save the output
+        start_date: first date that is included in the data
+        end_date: last date that is included in the data
+    """
+    # all periods in the FewsNet data
+    period_list = ["CS", "ML1", "ML2"]
+    df = gpd.GeoDataFrame()
+    # initialize progress bar
+    pbar = tqdm(dates)
+    # loop over dates
+    for d in pbar:
+        df_fews_list = []
+        for period in period_list:
+            pbar.set_description(f"Processing date {d}, period {period}")
+            # path to fewsnet data
+            # sometimes fewsnet publishes per region, sometimes per country
+            fews_path = None
+            fews_region_path = f"{folder_fews}{region}{d}/{regionabb}_{d}_{period}.shp"
+            fews_country_path = (
+                f"{folder_fews}{country_iso2}_{d}/{country_iso2}_{d}_{period}.shp"
+            )
+            if os.path.exists(fews_region_path):
+                fews_path = fews_region_path
+            elif os.path.exists(fews_country_path):
+                fews_path = fews_country_path
+
+            # path to population data
+            pop_path = f"{folder_pop}/{country_iso3.lower()}_ppp_{d[:4]}_1km_Aggregated_UNadj.tif"
+
+            if fews_path and os.path.exists(pop_path):
+                df_fews = merge_fewsnet_population(
+                    fews_path, admin_path, pop_path, d, period, shp_adm1c, shp_adm2c
+                )
+                df_fews_list.append(df_fews)
+            elif not fews_path:
+                logger.warning(
+                    f"FewsNet file for {d} and {period} not found. Skipping to next date and period."
+                )
+            elif not os.path.exists(pop_path):
+                logger.warning(
+                    f"Worldpop file for {d} not found. Skipping to next date"
+                )
+
+        if df_fews_list:
+            # concat the dfs of the different "periods", with an unique entry per date-adm1-adm2 combination
+            df_listind = [
+                df.set_index([shp_adm1c, shp_adm2c, "date"]) for df in df_fews_list
+            ]
+            df_comb = pd.concat(df_listind, axis=1).reset_index()
+
+            # add ipc level columns that are not present in the data (commonly level 5 columns)
+            ipc_cols = [f"{period}_{i}" for period in period_list for i in range(1, 6)]
+            for i in ipc_cols:
+                if i not in df_comb.columns:
+                    df_comb[i] = 0
+
+            # calculate total population of every admin, to use for comparison of population given by intersection of admin shape and fewsnet
+            df_adm = gpd.read_file(admin_path)
+            df_adm["pop"] = pd.DataFrame(
+                zonal_stats(vectors=df_adm["geometry"], raster=pop_path, stats="sum")
+            )["sum"]
+
+            # calculate population per period over all IPC levels
+            for period in period_list:
+                # population that has an IPC level assigned
+                df_comb[f"pop_{period}"] = df_comb[
+                    [f"{period}_{i}" for i in range(1, 6)]
+                ].sum(axis=1, min_count=1)
+                # all columns that contain "period", i.e. also the 99 valued columns
+                period_cols = [c for c in df_comb.columns if period in c]
+                # total population in the admin region that is included in the FewsNet data (i.e. also including the 99 values)
+                df_comb[f"pop_Total_{period}"] = df_comb[period_cols].sum(
+                    axis=1, min_count=1
+                )
+
+                # there can be a slight disperancy between the fewsnet and admin shapefile. Since we take the intersection, some areas might then be lost
+                # here we calculate the total population based on the admin shape, and compare it to the total population of the overlay of the admin and fewsnet shapefiles
+                # if the disperancy causes more than 5% of the population to be excluded, raise a warning
+                pop_admfews = (
+                    df_comb[f"pop_Total_{period}"].sum() / df_adm["pop"].sum() * 100
+                )
+
+                if pop_admfews < 95:
+                    logger.warning(
+                        f"For date {d} and period {d} only {pop_admfews:.2f}% of the country's population is included in the region covered by the FewsNet shapefile"
+                    )
+
+                # it can also be the case that FewsNet and Admin shapefile cover the same region
+                # but that FewsNet hasn't assigned a phase to a large part of the population
+                # if more than 50% doesn't have a phase assigned, raise a warning
+                perc_ipcclass = (
+                    df_comb[f"pop_{period}"].sum() / df_adm["pop"].sum() * 100
+                )
+                if perc_ipcclass < 50:
+                    logger.warning(
+                        f"For period {period} and date {d} only {perc_ipcclass:.2f}% of the population is assigned to an IPC class"
+                    )
+
+            df = df.append(df_comb, ignore_index=True)
+
+    if not df.empty:
+        # set general admin names
+        df.rename(columns={shp_adm1c: "ADMIN1", shp_adm2c: "ADMIN2"}, inplace=True)
+        # TODO: decide what kind of filename we want to use for the output, i.e. do we always want to overwrite the output or not
+        df.to_csv(
+            f"{result_folder}{country_iso3.lower()}_admin2_fewsnet_worldpop_{start_date}_{end_date}.csv"
+        )
+        # aggregate to admin1 by summing (and set to nan if no data for a date-adm1 combination
+        df_adm1 = (
+            df.drop("ADMIN2", axis=1)
+            .groupby(["date", "ADMIN1"])
+            .agg(lambda x: np.nan if x.isnull().all() else x.sum())
+            .reset_index()
+        )
+        df_adm1.rename(columns={"pop_ADMIN2": "pop_ADMIN1"}, inplace=True)
+        df_adm1.to_csv(
+            f"{result_folder}{country_iso3.lower()}_admin1_fewsnet_worldpop_{start_date}_{end_date}.csv"
+        )
+    else:
+        logger.warning("No data found for the given dates")
+
+
+def main(country_iso3, config_file="config.yml"):
+    """
+    This script computes the population per IPC phase per data - admin2 region combination.
+    The IPC phase is retrieved from the FewsNet data, which publishes their data in shapefiles, of three periods namely current situation (CS), near-term projection (ML1) and mid-term projection (ML2)
+    The IPC phases range from 1 to 5, and missing values are indicated by 99.
+    Args:
+        country_iso3: string with iso3 code
+        config_file: path to config file
+    """
+    parameters = parse_yaml(config_file)[country_iso3]
+
+    country = parameters["country_name"]
+    region = parameters["region"]
+    regioncode = parameters["regioncode"]
+    country_iso2 = parameters["iso2_code"]
+    dates = parameters["fewsnet_dates"]
+    admin2_shp = parameters["path_admin2_shp"]
+    shp_adm1c = parameters["shp_adm1c"]
+    shp_adm2c = parameters["shp_adm2c"]
+    start_date = parameters["start_date"]
+    end_date = parameters["end_date"]
+    # TODO: to make variables more generalizable with a config.py. Inspiration from pa-covid-model-parameterization
+    # pop_dir = os.path.join(config.DIR_PATH, country, config.POP_DIR)
+    FOLDER_FEWSNET = "Data/FewsNetRaw/"
+    FOLDER_POP = f"{country}/Data/WorldPop"
+    ADMIN2_PATH = f"{country}/Data/{admin2_shp}"
+    RESULT_FOLDER = f"{country}/Data/FewsNetWorldPop/"
+    # create output dir if it doesn't exist yet
+    Path(RESULT_FOLDER).mkdir(parents=True, exist_ok=True)
+    combine_fewsnet_projections(
+        country_iso3,
+        dates,
+        FOLDER_FEWSNET,
+        FOLDER_POP,
+        ADMIN2_PATH,
+        shp_adm1c,
+        shp_adm2c,
+        region,
+        regioncode,
+        country_iso2,
+        RESULT_FOLDER,
+        start_date,
+        end_date,
+    )
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    config_logger(level="warning")
+    main(args.country_iso3.upper())
---
+++ indicators/food_insecurity/process_globalipc.py
@@ -0,0 +1,102 @@
+import logging
+import numpy as np
+import pandas as pd
+import geopandas as gpd
+from pathlib import Path
+
+from utils import parse_args, parse_yaml, config_logger
+
+logger = logging.getLogger(__name__)
+
+
+def read_ipcglobal(parameters, ipc_path, shp_path, admin_level):
+    """
+    Process ipc data and do some checks
+    Args:
+        parameters: dict with parameters parsed from config
+        ipc_path: path to ipc data
+        shp_path: path to shapefile
+
+    Returns:
+        df_ipc: DataFrame with processed ipc data
+    """
+
+    # TODO: now assuming column names are already changed in the excel file. Might want to add something to change them automatically but fileformat is rather hard
+    # seems ipc file columns are always on line 11
+    df_ipc = pd.read_excel(ipc_path, header=[11])
+    # remove rows with nan date
+    df_ipc = df_ipc[
+        (df_ipc["date"].notnull()) & (df_ipc[f"ADMIN{admin_level}"].notnull())
+    ]
+
+    # replace values in ipc df
+    # mainly about differently spelled admin regions
+    if "replace_dict" in parameters:
+        replace_dict = parameters["replace_dict"]
+        df_ipc = df_ipc.replace(replace_dict)
+
+    if len(df_ipc[f"ADMIN{admin_level}"].dropna().unique()) == 0:
+        logger.warning(f"No admin {admin_level} regions found in the IPC file")
+
+    # TODO: implement other adm levels
+    if admin_level == 1:
+        df_ipc_agg = df_ipc.groupby(["ADMIN1", "date"], as_index=False).sum()
+    elif admin_level == 2:
+        df_ipc_agg = df_ipc.groupby(["date", "ADMIN1", "ADMIN2"], as_index=False).sum()
+    else:
+        df_ipc_agg = df_ipc.copy()
+
+    ipc_cols = [
+        f"{period}_{i}" for period in ["CS", "ML1", "ML2"] for i in [1, 2, 3, 4, 5]
+    ]
+    pop_cols = [f"pop_{period}" for period in ["CS", "ML1", "ML2"]]
+    # TODO: add ADMIN0
+    adm_cols = [f"ADMIN{a}" for a in range(1, int(admin_level) + 1)]
+
+    df_ipc_agg = df_ipc_agg[["date"] + adm_cols + ipc_cols + pop_cols]
+    # TODO: implement getting population per admin region, already implemented in proces_fewsnet.py
+    df_ipc_agg[f"pop_ADMIN{admin_level}"] = np.nan
+
+    shp_admc = parameters[f"shp_adm{admin_level}c"]
+    boundaries = gpd.read_file(shp_path)
+
+    # Check that admin level names in the IPC data are all reasonable
+    misspelled_names = np.setdiff1d(
+        list(df_ipc_agg[f"ADMIN{admin_level}"].dropna()),
+        list(boundaries[shp_admc].dropna()),
+    )
+    if misspelled_names.size > 0:
+        logger.warning(
+            f"The following admin {admin_level} regions from the IPC file are not found "
+            f"in the boundaries file: {misspelled_names}"
+        )
+
+    return df_ipc_agg
+
+
+def main(country_iso3, admin_level, config_file="config.yml"):
+    """
+    Define variables and save output
+    Args:
+        country_iso3: string with iso3 code
+        config_file: path to config file
+    """
+    parameters = parse_yaml(config_file)[country_iso3]
+    country = parameters["country_name"]
+    admin2_shp = parameters["path_admin2_shp"]
+    GLOBALIPC_PATH = parameters["ipc_path"]
+
+    SHP_PATH = f"{country}/Data/{admin2_shp}"
+    IPC_PATH = f"{country}/Data/{GLOBALIPC_PATH}"
+    RESULT_FOLDER = f"{country}/Data/GlobalIPCProcessed/"
+    # create output dir if it doesn't exist yet
+    Path(RESULT_FOLDER).mkdir(parents=True, exist_ok=True)
+
+    df_ipc = read_ipcglobal(parameters, IPC_PATH, SHP_PATH, admin_level)
+    df_ipc.to_csv(f"{RESULT_FOLDER}{country}_globalipc_ADMIN{admin_level}.csv")
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    config_logger(level="warning")
+    main(args.country_iso3.upper(), args.admin_level)
