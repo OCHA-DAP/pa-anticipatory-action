e522efeb57b2bd2928b1db4fd51b5955b83f0282
Tinkaa <tinkavalentijn@gmail.com>
2021-02-10 11:34:24 +0100

    add jan 2021 data and remove old things

--- analyses/ethiopia/scripts/eth_foodinsec_trigger.py
+++ analyses/ethiopia/scripts/eth_foodinsec_trigger.py
@@ -17,6 +17,7 @@ path_mod = f"{Path(os.path.dirname(os.path.realpath(__file__))).parents[2]}/"
 sys.path.append(path_mod)
 from indicators.food_insecurity.config import Config
 from indicators.food_insecurity.ipc_definemetrics import define_trigger_percentage, define_trigger_increase
+from indicators.food_insecurity.utils import compute_percentage_columns
 
 admin_level=1
 country="ethiopia"
@@ -33,31 +34,36 @@ globalipc_path=os.path.join(globalipc_dir,f"{country}_globalipc_admin{admin_leve
 adm_bound_path= os.path.join(country_folder,config.DATA_DIR,config.SHAPEFILE_DIR,parameters[f"path_admin{admin_level}_shp"])
 
 df_fn=pd.read_csv(os.path.join(fewsnet_dir,fewsnet_filename))
+#TODO: figure out a way to automatically add data to the updates in the FN processing script
+#the data of 2021-01 is an update and thus doesn't include CS data or projected periods
+#add them here manually, where the CS data is set to that of 2020-10
+CS_cols=[c for c in df_fn.columns if 'CS' in c]
+for c in CS_cols:
+    for a in df_fn[f"ADMIN{admin_level}"].unique():
+        df_fn.loc[(df_fn.date=="2021-01-01")&(df_fn[f"ADMIN{admin_level}"]==a),c]=df_fn.loc[(df_fn.date=="2020-10-01")&(df_fn[f"ADMIN{admin_level}"]==a),c].values
+df_fn[df_fn.date=="2021-01-01"]=compute_percentage_columns(df_fn[df_fn.date=="2021-01-01"],config)
+df_fn.loc[df_fn.date=="2021-01-01","period_ML1"]="Jan 2021"
+df_fn.loc[df_fn.date=="2021-01-01","period_ML2"]="Feb - May 2021"
 df_fn["source"]="FewsNet"
 df_gipc=pd.read_csv(globalipc_path)
 df_gipc["source"]="GlobalIPC"
+df_gipc["date"]=pd.to_datetime(df_gipc["date"])
 
 df=pd.concat([df_fn,df_gipc])
-df=df.replace(0,np.nan)
+
 df["date"]=pd.to_datetime(df["date"])
 df["year"]=df["date"].dt.year
 df["month"]=df["date"].dt.month
 df["trigger_ML1_4_20"]=df.apply(lambda x: define_trigger_percentage(x,"ML1",4,20),axis=1)
 df["trigger_ML1_3_30"]=df.apply(lambda x: define_trigger_percentage(x,"ML1",3,30),axis=1)
 df["trigger_ML1_3_5i"]=df.apply(lambda x: define_trigger_increase(x,"ML1",3,5),axis=1)
-df[f"threshold_reached_ML1"]=np.where((df[f"trigger_ML1_4_20"]==1) | ((df[f"trigger_ML1_3_30"]==1) & (df[f"trigger_ML1_3_5i"]==1)),1,0)
+df[f"threshold_reached_ML1"]=np.where((df[f"trigger_ML1_4_20"]==1) | ((df[f"trigger_ML1_3_30"]==1) & (df[f"trigger_ML1_3_5i"]==1)),True,False)
 
 df["trigger_ML2_4_20"]=df.apply(lambda x: define_trigger_percentage(x,"ML2",4,20),axis=1)
 df["trigger_ML2_3_30"]=df.apply(lambda x: define_trigger_percentage(x,"ML2",3,30),axis=1)
 df["trigger_ML2_3_5i"]=df.apply(lambda x: define_trigger_increase(x,"ML2",3,5),axis=1)
-df[f"threshold_reached_ML2"]=np.where((df[f"trigger_ML2_4_20"]==1) | ((df[f"trigger_ML2_3_30"]==1) & (df[f"trigger_ML2_3_5i"]==1)),1,0)
+df[f"threshold_reached_ML2"]=np.where((df[f"trigger_ML2_4_20"]==1) | ((df[f"trigger_ML2_3_30"]==1) & (df[f"trigger_ML2_3_5i"]==1)),True,False)
 
-df[df.date=="2020-10-01"]["period_ML1"]="Oct 2020 - Jan 2021"
-df[df.date=="2020-10-01"]["period_ML2"]="Feb 2021 - May 2021"
-
-# gdf=gpd.read_file(adm_bound_path).rename(columns={parameters[f"shp_adm{admin_level}c"]:f"ADMIN{admin_level}"})
-# df=gdf[[f"ADMIN{admin_level}","geometry"]].merge(df,how="right")
-# df=df[(df["source"]=="FewsNet") & (df["date"]==df["date"].max())]
 df.to_csv(os.path.join(config.DIR_PATH,"dashboard","data","foodinsecurity","ethiopia_foodinsec_trigger.csv"))
 
 # fig_boundbin=plot_spatial_binary_column(df,"trigger_ML1",subplot_col="year",subplot_str_col="year",region_col="ADMIN1",colp_num=4,only_show_reached=False,title_str="Regions triggered")
