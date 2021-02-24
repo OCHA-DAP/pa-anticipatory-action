6fd7adab13154cd8e8ae12325069ef657b7383b0
Josee Poirier <josee.phd@gmail.com>
2021-02-24 09:15:18 -0500

    fixed paths to shapefiles

--- analyses/malawi/scripts/mwi_chirps_dry_spell_detection.R
+++ analyses/malawi/scripts/mwi_chirps_dry_spell_detection.R
@@ -17,12 +17,12 @@ year_list <- data.frame(year = lubridate::year(seq.Date(from = as.Date("2010-01-
 ## load data 
 # set file paths
 data_dir <- Sys.getenv("AA_DATA_DIR")
-shapefile_path <- shapefiles_path <-  paste0(data_dir, "/raw/malawi/Shapefiles/mwi_adm_nso_20181016_shp")
+shapefile_path <- paste0(data_dir, "/raw/malawi/Shapefiles/mwi_adm_nso_20181016_shp")
 chirps_path <- paste0(data_dir, "/raw/drought/chirps")
 
 # read in country shapefiles
-mwi_adm2 <- st_read(paste0(shapefiles_path, "/mwi_admbnda_adm2_nso_20181016.shp"))
-mwi_adm3 <- st_read(paste0(shapefiles_path, "/mwi_admbnda_adm3_nso_20181016.shp"))
+mwi_adm2 <- st_read(paste0(shapefile_path, "/mwi_admbnda_adm2_nso_20181016.shp"))
+mwi_adm3 <- st_read(paste0(shapefile_path, "/mwi_admbnda_adm3_nso_20181016.shp"))
 
 # explore shapefiles
 summary(st_geometry_type(mwi_adm2)) # summary of geometry types
