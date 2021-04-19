91dbf5e121c1540929fa9c3b34828881d0de9bd8
Josee Poirier <josee.phd@gmail.com>
2021-04-19 17:31:18 -0400

    save food insecurity multi-plot graph

--- analyses/ethiopia/blog_post_graphs.R
+++ analyses/ethiopia/blog_post_graphs.R
@@ -10,13 +10,8 @@ fn_all <- read.csv(paste0(data_dir, "/processed/ethiopia/FewsNetAdmPop/ethiopia_
 
 eth_adm1 <- st_read(paste0(shapefile_path, "/eth_admbnda_adm1_csa_bofed_20201008.shp"))
 
-# subset vector data used to activate in December 2020
-fn <- fn_all %>%
-        filter(date == '2020-10-01') %>%
-        select(ADM1_EN, pop_CS, pop_ML1, pop_ML2, CS_3, CS_4, CS_5, ML1_3, ML1_4, ML1_5, ML2_3, ML2_4, ML2_5)
-
 # compute percentages
-fn <- fn %>%
+fn_all <- fn_all %>%
               mutate(perc_CS_3p = round((CS_3 + CS_4 + CS_5) * 100 / pop_CS, 1),
                      perc_CS_4p = round((CS_4 + CS_5) * 100 / pop_CS, 1),
                      perc_ML1_3p = round((ML1_3 + ML1_4 + ML1_5) * 100 / pop_ML1, 1),
@@ -24,16 +19,30 @@ fn <- fn %>%
                      period_ML1 = "Oct - Jan 2021",
                      perc_ML2_3p = round((ML2_3 + ML2_4 + ML2_5) * 100 / pop_ML2, 1),
                      perc_inc_ML2_3p = perc_ML2_3p - perc_CS_3p,
-                     period_ML2 = "Feb - May 2021")
-        
+                     period_ML2 = "Feb - May 2021",
+                     perc_ML1_4p = round((ML1_4 + ML1_5) * 100 / pop_ML1, 1),
+                     perc_inc_ML1_4p = perc_ML1_4p - perc_CS_4p,
+                     perc_ML2_4p = round((ML2_4 + ML2_5) * 100 / pop_ML2, 1),
+                     perc_inc_ML2_4p = perc_ML2_4p - perc_CS_4p)
 
-## plot food insecurity current and projected 
+# subset vector data used to activate in December 2020
+fn <- fn_all %>%
+  filter(date == '2020-10-01') %>%
+  select(ADM1_EN, pop_CS, pop_ML1, pop_ML2, CS_3, CS_4, CS_5, ML1_3, ML1_4, ML1_5, ML2_3, ML2_4, ML2_5)
+
+summary(fn) # note the absence of population in IPC4+        
+
+####### 
+### Generate plots
+####### 
+
+### IPC3+
 
 li = c(0, 100)
 br = c(0, 20, 40, 60, 80, 100)
 
 # Current situation
-fi_cs_plot <- eth_adm1[, c('ADM1_EN', 'geometry')] %>% 
+fi_cs_3p_plot <- eth_adm1[, c('ADM1_EN', 'geometry')] %>% 
                  left_join(fn, by = c('ADM1_EN' = 'ADM1_EN')) %>%
                  ggplot() +
                  geom_sf(aes(fill = perc_CS_3p), show.legend = F) +
@@ -53,7 +62,7 @@ fi_cs_plot <- eth_adm1[, c('ADM1_EN', 'geometry')] %>%
                        panel.grid.major = element_blank())
 
 # at ML1
-fi_ml1_plot <- eth_adm1[, c('ADM1_EN', 'geometry')] %>% 
+fi_ml1_3p_plot <- eth_adm1[, c('ADM1_EN', 'geometry')] %>% 
                 left_join(fn, by = c('ADM1_EN' = 'ADM1_EN')) %>%
                 ggplot() +
                 geom_sf(aes(fill = perc_ML1_3p), show.legend = F) +   
@@ -74,7 +83,7 @@ fi_ml1_plot <- eth_adm1[, c('ADM1_EN', 'geometry')] %>%
                       panel.grid.major = element_blank())
 
 # increase by ML1
-fi_inc_ml1_plot <- eth_adm1[, c('ADM1_EN', 'geometry')] %>% 
+fi_inc_ml1_3p_plot <- eth_adm1[, c('ADM1_EN', 'geometry')] %>% 
                     left_join(fn, by = c('ADM1_EN' = 'ADM1_EN')) %>%
                     ggplot() +
                     geom_sf(aes(fill = perc_inc_ML1_3p), show.legend = T) +            
@@ -93,7 +102,7 @@ fi_inc_ml1_plot <- eth_adm1[, c('ADM1_EN', 'geometry')] %>%
                           panel.grid.major = element_blank())
 
 # at ML2
-fi_ml2_plot <- eth_adm1[, c('ADM1_EN', 'geometry')] %>% 
+fi_ml2_3p_plot <- eth_adm1[, c('ADM1_EN', 'geometry')] %>% 
                   left_join(fn, by = c('ADM1_EN' = 'ADM1_EN')) %>%
                   ggplot() +
                   geom_sf(aes(fill = perc_ML2_3p), show.legend = T) +   
@@ -104,7 +113,7 @@ fi_ml2_plot <- eth_adm1[, c('ADM1_EN', 'geometry')] %>%
                                       "Percentage of \n regional population",
                                       breaks=br,
                                       limits=li) +
-                  ggtitle('Projected Feb-June 2021') +
+                  ggtitle('Projected for Feb-June 2021') +
                   #, subtitle = "Crisis or worse levels as reported by FewsNet") +
                   theme(axis.text.x=element_blank(),
                         axis.ticks.x=element_blank(),
@@ -113,15 +122,21 @@ fi_ml2_plot <- eth_adm1[, c('ADM1_EN', 'geometry')] %>%
                         panel.background = element_blank(),
                         panel.grid.major = element_blank())
 
-# assemble plots
-layout <- fi_cs_plot | fi_ml1_plot | fi_ml2_plot
-layout +
-   plot_annotation(
-    title = 'Crisis or Worse Food Insecurity (IPC3+)',
-    #subtitle = 'Crisis (IPC 3) or worse levels',
-    caption = 'Data source: FewsNet'
-  )
+#########
+#### create plots
+#########
 
-# plot current IPC3+, IPC3+ Jan, IPC3+ Feb+
+png(file = "blog_post_food_insecurity_graph.png",
+    width=1820, height=750)
+layout_3p <- fi_cs_3p_plot | fi_ml1_3p_plot | fi_ml2_3p_plot
+layout_3p +
+  plot_annotation(
+    title = 'Timeline of Food Insecurity in Ethiopia',
+    subtitle = 'Crisis (IPC 3) or worse levels',
+    caption = 'Data source: FewsNet',
+    theme = theme(plot.title = element_text(size = 18),
+                  plot.subtitle = element_text(size = 14))
+  ) 
+dev.off()
 
 
