f2d0afdb1a8f1164c7ad8e7427b53d686430b368
Tinkaa <tinkavalentijn@gmail.com>
2021-04-26 12:33:42 +0200

    improve anomaly plot

--- analyses/ssd/scripts/ssd_plot_monthly_precipitation.R
+++ analyses/ssd/scripts/ssd_plot_monthly_precipitation.R
@@ -6,24 +6,17 @@ library(tibble)
 library(lubridate)
 library(zoo)
 library(raster)
-# library(ggcorrplot)
 
 # -------------------------------------------------------------------------
-# Exploring agricultural stress and related impacts in Malawi
+# Exploring monthly rainfall in South Sudan
 # -------------------------------------------------------------------------
 
 
 # Setup -------------------------------------------------------------------
 
 data_dir <- Sys.getenv("AA_DATA_DIR")
-# shapefile_path <- paste0(data_dir, "/raw/ssd/Shapefiles/mwi_adm_nso_20181016_shp")
-# shapefile_path <- paste0(data_dir, "/raw/malawi/Shapefiles/mwi_adm_nso_20181016_shp")
-# 
-# shp_adm2 <- st_read(paste0(shapefile_path, "/mwi_admbnda_adm2_nso_20181016.shp"))
-# shp_adm1 <- st_read(paste0(shapefile_path, "/mwi_admbnda_adm1_nso_20181016.shp"))
-temp_dir = paste0(data_dir, '/processed/ssd/flooding/gee_output/')
+temp_dir = paste0(data_dir, '/processed/ssd/chirps/gee_output/')
 plot_dir = paste0(data_dir, "/processed/ssd/plots")
-# temp_dir = paste0(data_dir, '/processed/malawi/dry_spells/gee_output/')
 # Monthly precipitation ---------------------------------------------------
 
 precip_files <- list.files(path = temp_dir, pattern='SSD_adm1_ucsb-chg-chirps-daily_mean')
@@ -72,25 +65,51 @@ df_precip_all$anomaly <- df_precip_all$sum_precip - df_precip_all$month_avg_prec
 plt_precip <- ggplot(df_precip_all)+
   geom_bar(aes(x=date, y=sum_precip),fill='lightblue', stat='identity')+
   geom_line(aes(x=date, y=month_avg_precip), color='darkred', size=0.25)+
+  # facet_wrap(month) +
   #scale_color_discrete(labels = c("Total monthly precip", "Average total monthly precip"))+
   facet_grid(rows=vars(Region))+
   theme_bw()+
   labs(x='Date', y='Precipitation (mm)')+
   theme(legend.position = 'bottom',
         axis.text.y = element_text(size=10),)
-print(plt_precip)
+plt_precip
 
 # ggsave(paste0(plot_dir, '/chirps_monthly/ssd_monthly_precipitation_mean_1998_2020_adm1.png'), width = 20, height = 15, units = "in", dpi = 300)
 
-plt_precip <- ggplot(df_precip_all)+
-  geom_bar(aes(x=date, y=anomaly),fill='lightblue', stat='identity')+
+#convert month to numeric in order to filter on rainy season
+df_precip_all$month_num <- as.numeric(df_precip_all$month)
+df_precip_all$pos <- ifelse(df_precip_all$month_num %in% 7:10,ifelse(df_precip_all$anomaly >= 0, "positive anomaly", "negative anomaly"),"outside rainy season")
+df_precip_all$pos <- factor(df_precip_all$pos, levels = c("positive anomaly","negative anomaly","outside rainy season"))
+plt_precip_anom <- ggplot(df_precip_all)+
+  geom_bar(aes(x=date, y=anomaly,fill=pos),stat='identity')+
+  scale_fill_manual(values=c("#18998F","#C25048", "#cccccc"))+
   # geom_line(aes(x=date, y=month_avg_precip), color='darkred', size=0.25)+
   #scale_color_discrete(labels = c("Total monthly precip", "Average total monthly precip"))+
+  scale_x_date(date_breaks = "1 years",date_labels = "%Y")+
   facet_grid(rows=vars(Region))+
   theme_bw()+
   labs(x='Date', y='Precipitation anomaly (mm)')+
   theme(legend.position = 'bottom',
-        axis.text.y = element_text(size=10),)
-print(plt_precip)
+        axis.text.y = element_text(size=10),
+        axis.text.x = element_text(angle=90))
+plt_precip_anom
 
-# ggsave(paste0(plot_dir, '/chirps_monthly/ssd_monthly_precipitation_mean_1998_2020_adm1_anomaly.png'), width = 20, height = 15, units = "in", dpi = 300)
+ggsave(paste0(plot_dir, '/chirps_monthly/ssd_monthly_precipitation_mean_1998_2020_adm1_anomaly.png'), width = 20, height = 15, units = "in", dpi = 300)
+
+#for one region, to make it the info easier to process
+# df_precip_ce <- df_precip_all %>% filter(Region == "Central Equatoria")
+# flextable(df_precip_ce)
+# plt_precip_anom <- ggplot(df_precip_ce %>% filter(between(month, 7,10)))+
+#   geom_bar(aes(x=date, y=anomaly,fill=pos),stat='identity')+
+#   # geom_line(aes(x=date, y=month_avg_precip), color='darkred', size=0.25)+
+#   #scale_color_discrete(labels = c("Total monthly precip", "Average total monthly precip"))+
+#   scale_x_date(date_breaks = "1 years",date_labels = "%Y")+
+#   # facet_grid(rows=vars(month))+
+#   theme_bw()+
+#   labs(x='Date', y='Precipitation anomaly (mm)')+
+#   theme(legend.position = 'bottom',
+#         axis.text.y = element_text(size=10),
+#         axis.text.x = element_text(angle=90))
+# plt_precip_anom
+
+
