4f4adf1ce3ae9041c94d035dfa5190b9e57b7a55
Tinkaa <tinkavalentijn@gmail.com>
2021-05-03 11:57:44 +0200

    add hm without rainy season distinction

--- analyses/malawi/scripts/mwi_chirps_dry_spell_viz_hm_functions.R
+++ analyses/malawi/scripts/mwi_chirps_dry_spell_viz_hm_functions.R
@@ -82,3 +82,46 @@ plot_heatmap <- function(df_dry_spells,df_rainy_season, match_values,match_label
     scale_x_date(date_labels = "%b",date_breaks = "1 month",expand=c(0,0))
   return(hm_plot)
 }
+
+plot_heatmap_without_rainy <- function(df_dry_spells,df_rainy_season, match_values,match_labels,color_scale,y_label,plot_title,output_path,ds_flatdata=FALSE,sub_title=""){
+  #ds_flatdata: choose the appropriate preprocessing, depending on the format the data comes in
+  #the output from mwi_chirps_dry_spell_detection.R should use ds_flatdata=FALSE
+  #however, some cases the data is preprocessed in python in which case ds_flatdata=TRUE
+  if (ds_flatdata) {
+    df_ds <- prepare_ggplot(df_dry_spells,"dryspell_match")
+  }
+  else {
+    df_ds <- fill_dates(df_dry_spells, df_rainy_season,'dry_spell_first_date', 'dry_spell_last_date', 1)
+  }
+  # df_rs <- fill_dates(df_rainy_season, df_rainy_season,'onset_date', 'cessation_date', 10)
+  
+  theme_hm <- theme_minimal()+
+    theme(
+      axis.text.y=element_blank(),
+      axis.ticks.y=element_blank(),
+      legend.position = 'bottom',
+      strip.text = element_text(size=16,angle=0),
+      axis.text.x = element_text(size=16),
+      legend.text = element_text(size=16),
+      axis.title.x = element_text(size=16),
+      axis.title.y = element_text(size=16),
+      plot.title = element_text(size=32)
+    )
+  
+  
+  hm_plot <- df_ds %>%
+    # full_join(df_rs, by=c('pcode', 'dates'))%>%
+    # mutate(days = as.factor(day_type.x + day_type.y))%>%
+    mutate(days = factor(day_type, levels=dry_spell_match_values, labels=match_values_labels))%>%
+    drop_na(date_no_year) %>%
+    arrange(desc(pcode),date_no_year) %>%
+    ggplot(aes(x=date_no_year, y=pcode, fill=days))+
+    geom_tile() +
+    scale_fill_manual(values=color_scale)+
+    facet_grid(rows=vars(year))+
+    theme_minimal()+
+    labs(title=plot_title, subtitle=sub_title,x='Date', y=y_label, fill='')+
+    theme_hm+
+    scale_x_date(date_labels = "%b",date_breaks = "1 month",expand=c(0,0))
+  return(hm_plot)
+}
