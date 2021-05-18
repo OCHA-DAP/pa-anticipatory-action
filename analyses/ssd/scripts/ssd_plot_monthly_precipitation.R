adf5407709cab80571e2aa8114dbb49b6d1842b2
Tinkaa <tinkavalentijn@gmail.com>
2021-05-18 09:19:53 +0200

    add plot only jonglei

--- analyses/ssd/scripts/ssd_plot_monthly_precipitation.R
+++ analyses/ssd/scripts/ssd_plot_monthly_precipitation.R
@@ -17,15 +17,30 @@ library(flextable)
 
 data_dir <- Sys.getenv("AA_DATA_DIR")
 data_private_dir <- Sys.getenv("AA_DATA_PRIVATE_DIR")
-floodscan_dir <- paste0(data_private_dir, "/processed/ssd/floodscan")
-temp_dir = paste0(data_dir, '/processed/ssd/chirps/gee_output/')
-plot_dir = paste0(data_dir, "/processed/ssd/plots")
+floodscan_dir <- paste0(data_dir, "/private/processed/ssd/floodscan")
+temp_dir = paste0(data_dir, '/public/processed/ssd/chirps/gee_output/')
+plot_dir = paste0(data_dir, "/public/exploration/ssd/plots")
 # Monthly precipitation ---------------------------------------------------
 
 precip_files <- list.files(path = temp_dir, pattern='SSD_adm1_ucsb-chg-chirps-daily_mean')
 
 # precip_files <- list.files(path = temp_dir, pattern='mwi_adm1_ucsb-chg-chirps-daily')
 df_list_precip <- list()
+
+#settings for plots
+theme_hm <- theme_minimal()+
+  theme(
+    axis.text.y=element_blank(),
+    axis.ticks.y=element_blank(),
+    legend.position = 'bottom',
+    strip.text = element_text(size=16,angle=0),
+    axis.text.x = element_text(size=20,angle=90),
+    legend.text = element_text(size=24),
+    legend.title = element_text(size=24),
+    axis.title.x = element_text(size=20),
+    axis.title.y = element_text(size=20),
+    plot.title = element_text(size=32)
+  )
 
 for (i in 1:length(precip_files)){
 
@@ -81,9 +96,9 @@ df_precip_all$anomaly <- df_precip_all$sum_precip - df_precip_all$month_avg_prec
 # # ggsave(paste0(plot_dir, '/chirps_monthly/ssd_monthly_precipitation_mean_1998_2020_adm1.png'), width = 20, height = 15, units = "in", dpi = 300)
 # 
 # #convert month to numeric in order to filter on rainy season
-# df_precip_all$month_num <- as.numeric(df_precip_all$month)
-# df_precip_all$pos <- ifelse(df_precip_all$month_num %in% 7:10,ifelse(df_precip_all$anomaly >= 0, "positive anomaly", "negative anomaly"),"outside rainy season")
-# df_precip_all$pos <- factor(df_precip_all$pos, levels = c("positive anomaly","negative anomaly","outside rainy season"))
+df_precip_all$month_num <- as.numeric(df_precip_all$month)
+df_precip_all$pos <- ifelse(df_precip_all$month_num %in% 7:10,ifelse(df_precip_all$anomaly >= 0, "positive anomaly", "negative anomaly"),"outside rainy season")
+df_precip_all$pos <- factor(df_precip_all$pos, levels = c("positive anomaly","negative anomaly","outside rainy season"))
 # plt_precip_anom <- ggplot(df_precip_all)+
 #   geom_bar(aes(x=date, y=anomaly,fill=pos),stat='identity')+
 #   scale_fill_manual(values=c("#18998F","#C25048", "#cccccc"))+
@@ -98,6 +113,22 @@ df_precip_all$anomaly <- df_precip_all$sum_precip - df_precip_all$month_avg_prec
 #         axis.text.x = element_text(angle=90))
 # plt_precip_anom
 
+#plot only one admin for presentation graph
+plt_precip_anom_jonglei <- ggplot(df_precip_all %>% filter(Region=="Jonglei"))+
+  geom_bar(aes(x=date, y=anomaly,fill=pos),stat='identity')+
+  scale_fill_manual(values=c("#18998F","#C25048", "#cccccc"))+
+  # geom_line(aes(x=date, y=month_avg_precip), color='darkred', size=0.25)+
+  #scale_color_discrete(labels = c("Total monthly precip", "Average total monthly precip"))+
+  scale_x_date(date_breaks = "1 years",date_labels = "%Y")+
+  facet_grid(cols=vars(Region))+
+  theme_bw()+
+  theme_hm+
+  labs(x='Date', y='Precipitation anomaly (mm)')+
+  theme(legend.position = 'bottom',
+        axis.text.y = element_text(size=10),
+        axis.text.x = element_text(angle=90))
+plt_precip_anom_jonglei
+ggsave(paste0(plot_dir, '/chirps_monthly/ssd_monthly_precipitation_mean_1998_2020_jonglei_anomaly.png'), plot=plt_precip_anom_jonglei, width = 14, height = 10, units = "in", dpi = 300)
 
 
 
@@ -117,7 +148,7 @@ df_fs$rainy_season <- ifelse(as.numeric(df_fs$month) %in% 7:10,"rainy","not rain
 
 df_fs_month <- df_fs %>% 
   group_by(month,year, ADM1_EN) %>%
-  summarise(max_cell_month = max(max_cell),mean_cell_month = max(mean_cell),percentile_2_month=max(percentile_2)) %>% 
+  summarise(max_cell_month = max(max_cell),mean_cell_month = max(mean_cell),percentile_10_month=max(percentile_10)) %>% 
   mutate(date = lubridate::as_date(paste0(year, month, '01'), format='%Y%m%d'))
 df_fs_month$rainy_season <- ifelse(as.numeric(df_fs_month$month) %in% 7:10,"yes","no")
 
@@ -133,40 +164,56 @@ df_fs_month <- df_fs_month[order(df_fs_month$year,df_fs_month$month),]
 #   scale_x_date(date_labels = "%Y") #+
 # plt_fs
 # 
-# plt_fs <- ggplot(df_fs_month, aes(x=date, y=mean_cell_month,color=rainy_season))+#, group=ADM1_EN)) +
-#   scale_color_manual(values=c(yes="#C25048", no="#cccccc"))+#(values = c(apple = "red", avocado = "green", ...)
-#   facet_wrap(~ADM1_EN)+
-#   geom_line(aes(group = 1))+
-#   labs(y='mean fraction of cell flooded', x='Date')+
-#   theme_bw()+
-#   theme(legend.position = 'bottom')+
-#   scale_x_date(date_labels = "%Y") 
-# plt_fs
-
-plt_fs <- ggplot(df_fs_month, aes(x=date, y=percentile_2_month,color=rainy_season))+#, group=ADM1_EN)) +
-  scale_color_manual(values=c(yes="#C25048", no="#cccccc"))+#(values = c(apple = "red", avocado = "green", ...)
+plt_fs <- ggplot(df_fs_month, aes(x=date, y=mean_cell_month,color=rainy_season))+
+  scale_color_manual(values=c(yes="#C25048", no="#cccccc"))+
   facet_wrap(~ADM1_EN)+
   geom_line(aes(group = 1))+
   labs(y='mean fraction of cell flooded', x='Date')+
   theme_bw()+
   theme(legend.position = 'bottom')+
-  scale_x_date(date_labels = "%Y") 
+  scale_x_date(date_labels = "%Y")
 plt_fs
 
-df_fs_bar <- ggplot(df_fs_month)+
-  geom_bar(aes(x=date, y=mean_cell_month,fill=rainy_season),stat='identity')+
-  scale_fill_manual(values=c(yes="#C25048", no="#cccccc"))+
-  # geom_line(aes(x=date, y=month_avg_precip), color='darkred', size=0.25)+
-  #scale_color_discrete(labels = c("Total monthly precip", "Average total monthly precip"))+
-  scale_x_date(date_breaks = "1 years",date_labels = "%Y")+
-  # facet_grid(rows=vars(ADM1_EN))+
+#only plot one region, for presentation graph
+plt_fs_jonglei <- ggplot(df_fs_month %>% filter(ADM1_EN=="Jonglei"), aes(x=date, y=mean_cell_month,color=rainy_season))+#, group=ADM1_EN)) +
+  scale_color_manual(values=c(yes="#007CE0", no="#cccccc"))+
   facet_wrap(~ADM1_EN)+
+  geom_line(aes(group = 1))+
+  labs(y='mean fraction of cell flooded', x='Date')+
   theme_bw()+
-  labs(x='Date', y='Precipitation anomaly (mm)')+
-  theme(legend.position = 'bottom',
-        axis.text.y = element_text(size=10),
-        axis.text.x = element_text(angle=90))
-df_fs_bar
+  theme_hm+
+  theme(legend.position = 'bottom')+
+  scale_x_date(date_breaks = "1 years", date_labels = "%Y")
+plt_fs_jonglei
+
+ggsave(paste0(plot_dir, '/floodscan/ssd_floodscan_mean_1998_2020_jonglei.png'), plot=plt_fs_jonglei, width = 10, height = 8, units = "in", dpi = 300)
+
+
+
+# plt_fs <- ggplot(df_fs_month, aes(x=date, y=percentile_10_month,color=rainy_season))+#, group=ADM1_EN)) +
+#   scale_color_manual(values=c(yes="#C25048", no="#cccccc"))+#(values = c(apple = "red", avocado = "green", ...)
+#   facet_wrap(~ADM1_EN)+
+#   geom_line(aes(group = 1))+
+#   labs(y='percentile fraction of cell flooded', x='Date')+
+#   theme_bw()+
+#   theme(legend.position = 'bottom')+
+#   scale_x_date(date_labels = "%Y") 
+# plt_fs
+
+# df_fs_bar <- ggplot(df_fs_month)+
+#   geom_bar(aes(x=date, y=mean_cell_month,fill=rainy_season),stat='identity')+
+#   scale_fill_manual(values=c(yes="#C25048", no="#cccccc"))+
+#   # geom_line(aes(x=date, y=month_avg_precip), color='darkred', size=0.25)+
+#   #scale_color_discrete(labels = c("Total monthly precip", "Average total monthly precip"))+
+#   scale_x_date(date_breaks = "1 years",date_labels = "%Y")+
+#   # facet_grid(rows=vars(ADM1_EN))+
+#   facet_wrap(~ADM1_EN)+
+#   theme_bw()+
+#   labs(x='Date', y='Precipitation anomaly (mm)')+
+#   theme(legend.position = 'bottom',
+#         axis.text.y = element_text(size=10),
+#         axis.text.x = element_text(angle=90))
+# df_fs_bar
 
 
 # plt_fs_month <- ggplot(df_fs_month, aes(x=date, y=max_cell_month))+#, group=ADM1_EN)) +
