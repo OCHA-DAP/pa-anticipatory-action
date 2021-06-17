9abd01c8f6795406c81ad882faf31e93d4036d82
turnerm <turnermon@gmail.com>
2021-06-17 08:34:35 +0200

    Moved cds test back to GloFAS file

--- tests/test_glofas.py
+++ tests/test_glofas.py
@@ -7,6 +7,24 @@ from src.indicators.flooding.ecmwf import ecmwf
 from src.indicators.flooding.cds import cds
 from src.indicators.flooding.cds.area import Area
 
+import numpy as np
+import xarray as xr
+
+
+def test_expand_dims():
+    rs = np.random.RandomState(12345)
+    size_x, size_y = (10, 20)
+    ds = xr.Dataset(
+        data_vars={"var_a": (("x", "y"), rs.rand(size_x, size_y))},
+        coords={"x": np.arange(size_x), "y": np.arange(size_y)},
+    )
+    ds.coords["z"] = 1
+    assert "z" not in ds.dims.keys()
+    ds = cds.expand_dims(
+        ds=ds, dataset_name="var_a", coord_names=["z", "x", "y"], expansion_dim=0
+    )
+    assert "z" in ds.dims.keys()
+
 
 @mock.patch("src.indicators.flooding.cds.cds.cdsapi.Client.retrieve")
 @mock.patch("src.indicators.flooding.cds.cds.Path.mkdir")
