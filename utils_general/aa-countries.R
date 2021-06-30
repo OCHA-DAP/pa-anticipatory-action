15088685dade508b7cfcdcf710b7ed65d43f88e1
Josee Poirier <josee.phd@gmail.com>
2021-06-30 16:45:12 -0400

    addressed comments from PR

--- utils_general/aa-countries.R
+++ utils_general/aa-countries.R
@@ -1,17 +1,17 @@
 ###
 # Install packages
 ###
-
 library(tidyverse)
-#library(rvest)
-#library(magrittr)
-#library(ggmap)
-#library(stringr)
+
+###
+# set up paths
+###
+data_dir <- Sys.getenv("AA_DATA_DIR")
+plot_path <- paste0(data_dir, "/public/processed/glb/plots")
 
 ###
 # get world map
 ###
-
 world_map <- map_data("world")
 
 ###
@@ -20,15 +20,12 @@ world_map <- map_data("world")
 
 # list countries and shocks
 drought <- c("Somalia", "Ethiopia", "Niger", "Burkina Faso")
-#dry_spells <- c("Malawi")
-#floods <- c("Bangladesh", "Nepal", "Malawi", "Chad", "South Sudan")
 floods <- c("Bangladesh", "Nepal", "Chad", "South Sudan")
 dual <- c("Malawi")
 storms <- c("Philippines") # There is no "The" in the world_map
 infectious <- c("Madagascar")
 
 aa_countries_list <- data.frame(country = drought, shock = "Drought") 
-#aa_countries_list <- rbind(aa_countries_list, data.frame(country = dry_spells, shock = "dry_spells"))
 aa_countries_list <- rbind(aa_countries_list, data.frame(country = dual, shock = "Dry Spells + Floods"))
 aa_countries_list <- rbind(aa_countries_list, data.frame(country = floods, shock = "Floods"))
 aa_countries_list <- rbind(aa_countries_list, data.frame(country = storms, shock = "Storms"))
@@ -54,22 +51,23 @@ aa_countries_map <-  world_map %>% filter(!is.na(shock)) # subset mapping + shoc
 # select colors for each shock, in order: drought, dry spells + floods, floods, infectious, storms
 colours <- gplots::col2hex(c("lightgoldenrod2", "springgreen4" , "dodgerblue3", "chocolate2", "lightskyblue"))
 
+# plot map and colorise AA countries
 ggplot(world_map, aes(x = long, y = lat, group = group)) +
   geom_polygon(fill = "#CCCCCC") +
-  #geom_polygon(data = world_map, aes(x = long, y = lat, group = group, fill = aa_country), show.legend = TRUE) +
   geom_polygon(data = aa_countries_map, aes(x = long, y = lat, group = group, fill = shock), show.legend = TRUE) +
   scale_fill_manual(values =  colours, name = "Shocks") +
   labs(title = 'Countries with Anticipatory Action Frameworks'
        ,subtitle = "Active and under development frameworks") +
   theme(legend.position="bottom"
-        #text = element_text(family = "Gill Sans", color = "#FFFFFF")
         ,plot.title = element_text(size = 24)
         ,plot.subtitle = element_text(size = 12)
         ,axis.text = element_blank()
         ,axis.title = element_blank()
         ,axis.ticks = element_blank()
         ,panel.background = element_rect(fill = "white")
-        #,plot.background = element_rect(fill = "#444444")
         ,panel.grid = element_blank()
   )
+
+# save plot ## NOTE Programmatically saving the image distorts it. Better save through the GUI for now until we figure out the issue.
+ggsave(paste0(plot_path, "/aa_countries_map.png"), width = 1426, height = 744, units = "px", dpi = 300)
 
