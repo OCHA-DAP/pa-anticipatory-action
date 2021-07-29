1b823f5fd97aff5aa76bb998acd0d99f7d677973
turnerm <turnermon@gmail.com>
2021-07-29 14:17:06 +0200

    Added Bangladesh

--- run_glofas.py
+++ run_glofas.py
@@ -5,7 +5,7 @@ from pathlib import Path
 import geopandas as gpd
 
 from src.indicators.flooding.glofas import glofas
-from src.indicators.flooding.glofas.area import AreaFromShape, Station
+from src.indicators.flooding.glofas import area
 from src.utils_general.utils import parse_yaml
 from src.utils_general import admin
 
@@ -13,12 +13,12 @@ from src.utils_general import admin
 logging.basicConfig(level=logging.INFO, force=True)
 logger = logging.getLogger(__name__)
 
-CONFIG_DIR = Path("country_config")
+CONFIG_DIR = Path("src/country_config")
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("country-iso3", help="country ISO3")
+    parser.add_argument("country_iso3", help="country ISO3")
     return parser.parse_args()
 
 
@@ -27,9 +27,13 @@ def main(country_iso3, download=True, process=True):
     config = parse_yaml(CONFIG_DIR / f"{country_iso3}.yml")
     config_glofas = config["glofas"]
     leadtimes = [x + 1 for x in range(config_glofas["leadtimes"]["max"])]
+    stations = {
+        name: area.Station(lon=coords["lon"], lat=coords["lat"])
+        for name, coords in config_glofas["stations"].items()
+    }
 
-    glofas_reanalysis = glofas.GlofasReanalysis(**config_glofas["kwargs"])
-    glofas_reforecast = glofas.GlofasReforecast(**config_glofas["kwargs"])
+    glofas_reanalysis = glofas.GlofasReanalysis()
+    glofas_reforecast = glofas.GlofasReforecast()
 
     if download:
         if config_glofas["area_type"] == "shapefile":
@@ -40,32 +44,36 @@ def main(country_iso3, download=True, process=True):
                 base_shapefile=config["admin"]["base_shapefile"],
             )
             df_admin_boundaries = gpd.read_file(f"zip://{shapefile}")
-            area = AreaFromShape(df_admin_boundaries.iloc[0]["geometry"])
+            download_area = area.AreaFromShape(
+                df_admin_boundaries.iloc[0]["geometry"]
+            )
+        elif config_glofas["area_type"] == "station":
+            download_area = area.AreaFromStations(stations=stations)
+        else:
+            logger.error(
+                "Parameter 'area_type' needs to be one of "
+                "'shpaefile' or 'station'"
+            )
+            return -1
         glofas_reanalysis.download(
-            country_iso3=country_iso3, area=area, **config_glofas["kwargs"]
+            country_iso3=country_iso3, area=download_area
         )
         glofas_reforecast.download(
             country_iso3=country_iso3,
-            area=area,
+            area=download_area,
             leadtimes=leadtimes,
-            **config_glofas["kwargs"],
         )
 
     if process:
-        stations = {
-            name: Station(lon=coords["lon"], lat=coords["lat"])
-            for name, coords in config_glofas["stations"].items()
-        }
+
         glofas_reanalysis.process(
             country_iso3=country_iso3,
             stations=stations,
-            **config_glofas["kwargs"],
         )
         glofas_reforecast.process(
             country_iso3=country_iso3,
             stations=stations,
             leadtimes=leadtimes,
-            **config_glofas["kwargs"],
         )
 
 
