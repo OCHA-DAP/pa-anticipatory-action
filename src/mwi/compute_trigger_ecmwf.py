66a51329ba252a84045d2e30d28c71303e758bf5
Tinkaa <tinkavalentijn@gmail.com>
2021-09-21 09:41:41 +0300

    improve logic

--- src/mwi/compute_trigger_ecmwf.py
+++ src/mwi/compute_trigger_ecmwf.py
@@ -1,23 +1,17 @@
-# notes:
-# we are now first merging all grib files to one nc
-# and using that nc to compute admin stats
-# it might be neater and more efficient to convert
-# each grib file to a separate nc instead
-
 import logging
 import os
 import sys
 from typing import List
 from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
-
-# TODO: remove this after making top-level
 from pathlib import Path
 
 import geopandas as gpd
 import numpy as np
 import pandas as pd
 
+# TODO: remove this after making top-level
 path_mod = f"{Path(os.path.dirname(os.path.realpath(__file__))).parents[1]}/"
 sys.path.append(path_mod)
 from src.indicators.drought.config import Config
@@ -32,12 +26,37 @@ logging.basicConfig(level=logging.INFO, force=True)
 logger = logging.getLogger(__name__)
 
 COUNTRY_ISO3 = "mwi"
+# version number of the trigger
+# this script is written for v1
+VERSION = 1
 CONFIG = Config()
 PARAMETERS = CONFIG.parameters(COUNTRY_ISO3)
 
 COUNTRY_DATA_RAW_DIR = (
     Path(CONFIG.DATA_DIR) / CONFIG.PUBLIC_DIR / CONFIG.RAW_DIR / COUNTRY_ISO3
 )
+
+
+COUNTRY_DATA_PROCESSED_DIR = (
+    Path(CONFIG.DATA_DIR)
+    / CONFIG.PUBLIC_DIR
+    / CONFIG.PROCESSED_DIR
+    / COUNTRY_ISO3
+)
+
+
+def get_ouput_path(
+    iso3: str, version: int, target_year: str, target_month: str
+):
+    directory = (
+        COUNTRY_DATA_PROCESSED_DIR
+        / CONFIG.DRY_SPELLS_DIR
+        / f"v{version}"
+        / CONFIG.TRIGGER_METRICS_DIR
+        / "predictive_trigger"
+    )
+    filename = f"{iso3}_predictive_trigger_{target_year}{target_month}.csv"
+    return directory / filename
 
 
 def retrieve_forecast(
@@ -55,14 +74,14 @@ def retrieve_forecast(
     # but not important in this case as we just want some extra area
     area = AreaFromShape(gdf_bound.buffer(3))
 
-    year_start = target_date.year + (
-        (target_date.month - max(leadtimes)) // 12
-    )
+    year_start = (target_date - relativedelta(months=6)).year
 
-    months = [(target_date.month - lt) % 12 for lt in leadtimes]
+    months = [
+        (target_date - relativedelta(months=lt)).month for lt in leadtimes
+    ]
 
     # this will download the months for year_start and target_date.year
-    # so some duplication, but that is okay
+    # so some of that data might not be needed, but that is okay
     ecmwf_forecast.download(
         country_iso3=iso3,
         area=area,
@@ -70,10 +89,8 @@ def retrieve_forecast(
         year_max=target_date.year,
         months=months,
     )
-    # print(os.path.getmtime(ecmwf_seasonal.get_raw)
-    # # #todo: would be nice to only do process if new data is downloaded
-    # ecmwf_forecast.process(country_iso3=iso3)
-    # do not use cache as new leadtimes can be added
+
+    ecmwf_forecast.process(country_iso3=iso3)
     compute_stats_per_admin(
         iso3=iso3,
         interpolate=False,
@@ -81,26 +98,26 @@ def retrieve_forecast(
         adm_level=adm_level,
         pcode_col=pcode_col,
         add_col=add_col,
+        # do not use cache as new leadtimes can be added
         use_cache=False,
     )
 
 
 def compute_trigger(
-    iso3,
+    iso3: str,
     target_date: datetime,
-    min_prob,
-    precip_cap,
+    min_prob: float,
+    precip_cap: int,
+    download: bool = True,
     interpolate_raster=False,
     leadtimes: List[int] = None,
     pcodes: List[str] = None,
-    adm_level=1,
-    aggr_meth="mean_ADM1_PCODE",
-    pcode_col="ADM1_PCODE",
-    adm_name_col="ADM1_EN",
-    date_col="date",
-    leadtime_col="leadtime",
-    # todo: check more general debug flag
-    debug=False,
+    adm_level: int = 1,
+    aggr_meth: str = "mean_ADM1_PCODE",
+    pcode_col: str = "ADM1_PCODE",
+    adm_name_col: str = "ADM1_EN",
+    date_col: str = "date",
+    leadtime_col: str = "leadtime",
 ):
 
     adm_bound_path = (
@@ -110,14 +127,16 @@ def compute_trigger(
     )
     gdf_adm = gpd.read_file(adm_bound_path)
 
-    retrieve_forecast(
-        iso3,
-        gdf_bound=gdf_adm,
-        target_date=target_date,
-        adm_level=adm_level,
-        pcode_col=pcode_col,
-        add_col=[adm_name_col],
-    )
+    if download:
+        retrieve_forecast(
+            iso3,
+            gdf_bound=gdf_adm,
+            target_date=target_date,
+            adm_level=adm_level,
+            pcode_col=pcode_col,
+            leadtimes=range(0, 7) if leadtimes is None else leadtimes,
+            add_col=[adm_name_col],
+        )
 
     stats_filename = get_stats_filepath(
         iso3,
@@ -148,19 +167,25 @@ def compute_trigger(
     df_stats_quant["trigger_met"] = np.where(
         df_stats_quant[f"below_{precip_cap}"] == 1, True, False
     )
-    if not debug:
-        df_stats_quant = df_stats_quant[
-            [
-                "date_month",
-                pcode_col,
-                adm_name_col,
-                leadtime_col,
-                aggr_meth,
-                f"below_{precip_cap}",
-                "trigger_met",
-            ]
+    logging.debug(df_stats_quant)
+    df_stats_quant = df_stats_quant[
+        [
+            "date_month",
+            pcode_col,
+            adm_name_col,
+            leadtime_col,
+            aggr_meth,
+            f"below_{precip_cap}",
+            "trigger_met",
         ]
-    print(df_stats_quant)
+    ]
+
+    output_path = get_ouput_path(
+        iso3, VERSION, target_date.year, target_date.month
+    )
+    Path(output_path.parent).mkdir(parents=True, exist_ok=True)
+    df_stats_quant.to_csv(output_path, index=False)
+
     return df_stats_quant
 
 
@@ -200,14 +225,13 @@ def compute_trigger(
 
 
 def main():
-    interpolate_raster = False
-    # todo: check if better method than pd.to_datetime
     compute_trigger(
         COUNTRY_ISO3,
-        pd.to_datetime("2020-01-01"),
-        0.5,
-        210,
-        interpolate_raster=interpolate_raster,
+        target_date=pd.to_datetime("2020-01-01"),
+        min_prob=0.5,
+        precip_cap=210,
+        download=False,
+        interpolate_raster=False,
     )
 
 
