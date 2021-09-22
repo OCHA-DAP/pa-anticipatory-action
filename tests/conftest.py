002e5b18473a5a0fdbce21540070ba8f3659306b
Mike <rans@un.org>
2021-09-22 15:17:05 +1200

    Only delete temp folder on success

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,4 @@
-from os import makedirs, environ
+from os import environ, mkdir
 from os.path import join, exists
 from shutil import rmtree
 from tempfile import gettempdir
@@ -13,7 +13,7 @@ TMP_PATH = join(gettempdir(), "test_anticipatory_action")
 def all_tests_initialise(request, session_mocker):
     if exists(TMP_PATH):
         rmtree(TMP_PATH)
-    makedirs(TMP_PATH)
+    mkdir(TMP_PATH)
     session_mocker.patch.dict(environ, {"AA_DATA_DIR": TMP_PATH})
     yield
     if request.node.testsfailed == 0:
