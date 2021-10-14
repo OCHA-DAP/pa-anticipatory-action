16f25c67affd7c6811cc8df8950c27dadd023fa9
turnerm <turnermon@gmail.com>
2021-10-14 16:43:20 +0200

    Add test changes

---
+++ tests/conftest.py
@@ -0,0 +1,19 @@
+from os import environ, mkdir
+from os.path import exists, join
+from shutil import rmtree
+from tempfile import gettempdir
+
+import pytest
+
+TMP_PATH = join(gettempdir(), "test_anticipatory_action")
+
+
+@pytest.fixture(scope="session", autouse=True)
+def all_tests_initialise(request, session_mocker):
+    if exists(TMP_PATH):
+        rmtree(TMP_PATH)
+    mkdir(TMP_PATH)
+    session_mocker.patch.dict(environ, {"AA_DATA_DIR": TMP_PATH})
+    yield
+    if request.node.testsfailed == 0:
+        rmtree(TMP_PATH)
