a3210c5d3fc13fa74094f81c18eb44c582429729
Seth Caldwell <caldwellst@gmail.com>
2021-11-26 14:20:09 +0000

    Implement downloading all raw data for the WRSI

--- src/indicators/drought/wrsi.py
+++ src/indicators/drought/wrsi.py
@@ -27,23 +27,120 @@ experts on the differences between the two, how they relate,
 and how best to use them.
 """
 
+import logging
 import os
-from typing import Literal, get_args
+from datetime import date
+from io import BytesIO
+from pathlib import Path
+from typing import Literal, Union, get_args
+from urllib.error import HTTPError
+from urllib.request import urlopen
+from zipfile import ZipFile
+
+logger = logging.getLogger(__name__)
 
 _base_url = "https://edcftp.cr.usgs.gov/project/fews/dekadal/africa_west/"
 
-_raw_zip_filename = "{year}{dekad}{region}.zip"
+_url_zip_filename = "w{year:02}{dekad}{region}.zip"
+
+_base_file_name = "w{year:02}{dekad}{type}.tif"
+
+
+def _get_url_path(year, dekad, region, historical=False):
+    url_zip_filename = _url_zip_filename.format(
+        year=year % 100, dekad=dekad, region=region
+    )
+    historical = "historical/" if historical else ""
+    return _base_url + historical + url_zip_filename
+
+
+def _download_wrsi(year: int, dekad: int, region: str, file_dir: Path):
+    try:
+        url = _get_url_path(year=year, dekad=dekad, region=region)
+        resp = urlopen(url)
+    except HTTPError:
+        try:
+            url = _get_url_path(
+                year=year, dekad=dekad, region=region, historical=True
+            )
+            resp = urlopen(url)
+        except HTTPError:
+            logger.error(
+                f"No data available for region {region} for "
+                f"dekad {dekad} of {year}, skipping."
+            )
+            return
+
+    zf = ZipFile(BytesIO(resp.read()))
+    for type in ["do", "eo"]:
+        file_name = _base_file_name.format(
+            year=year % 100, dekad=dekad, type=type
+        )
+        save_path = os.path.join(file_dir, file_name)
+        if os.path.exists(save_path):
+            os.remove(save_path)
+
+        zf.extract(file_name, file_dir)
+
 
 _raw_path = os.path.join(
-    os.getenv("AA_DATA_DIR"), "public", "raw", "general", "wrsi"
+    os.getenv("AA_DATA_DIR"), "public", "raw", "general", "wrsi", "west_africa"
 )
 
 
-def _get_raw_path(year, dekad, region):
-    return os.path.join(
-        _raw_path,
-        _raw_zip_filename.format(year=year, dekad=dekad, region=region),
-    )
+def _get_raw_dir(region):
+    return Path(os.path.join(_raw_path, region))
+
+
+def _get_year_dekad(date_obj: Union[date, str]):
+    if isinstance(date_obj, str):
+        date_obj = date.fromisoformat(date_obj)
+    year = date_obj.year
+    dekad = (date_obj.timetuple().tm_yday // 10) + 1
+    return year, dekad
+
+
+RegionArgument = Literal["cropland", "rangeland"]
+_valid_region = get_args(RegionArgument)
+
+
+def download_wrsi(
+    region: RegionArgument,
+    start_date: Union[date, str, None] = None,
+    end_date: Union[date, str, None] = None,
+):
+    if region not in _valid_region:
+        raise ValueError("`region` must be one of 'cropland' or 'rangeland'.")
+    else:
+        url_region = "wa" if region == "cropland" else "w1"
+
+    if start_date is None:
+        start_year, start_dekad = 2001, 13
+        if region == "rangeland":
+            start_year += 1
+    else:
+        start_year, start_dekad = _get_year_dekad(start_date)
+
+    end_date = date.today() if end_date is None else end_date
+    end_year, end_dekad = _get_year_dekad(end_date)
+    end_dekad -= 1
+
+    i_year = start_year
+    i_dekad = min(start_dekad, 33)
+
+    file_dir = _get_raw_dir(region)
+    file_dir.mkdir(parents=True, exist_ok=True)
+
+    while not (
+        i_year > end_year or (i_year == end_year and i_dekad > i_dekad)
+    ):
+        _download_wrsi(
+            year=i_year, dekad=i_dekad, region=url_region, file_dir=file_dir
+        )
+        i_dekad += 1
+        if i_dekad > 33:
+            i_year += 1
+            i_dekad = 13
 
 
 _processed_filename = "biomasse_{admin}_dekad_{start_dekad}.csv"
@@ -58,7 +155,3 @@ def _get_processed_path(admin, start_dekad):
         _processed_path,
         _processed_filename.format(admin=admin, start_dekad=start_dekad),
     )
-
-
-AdminArgument = Literal["ADM0", "ADM1", "ADM2"]
-_valid_admin = get_args(AdminArgument)
