0267033743ec2a2f6e3ed5a720332726029ff3f8
Josee Poirier <josee.phd@gmail.com>
2022-04-06 11:40:57 -0400

    added cessation dates and season durations

--- analyses/mwi/dryspells_trigger/exploration/2021_2022_postseason_data_analysis.R
+++ analyses/mwi/dryspells_trigger/exploration/2021_2022_postseason_data_analysis.R
@@ -41,16 +41,14 @@ lf <- data %>%
 # analysis
 #####
 
-## TO DO handle rainfall = NAs ****
-
 ## 
 # Rainy season stats
 ##
 
 # identify streaks of rainy days per cell (streak minimum: 1 day)
-stats <- lf %>%
+streaks <- lf %>%
         group_by(cell) %>%
-        arrange(dates) %>%
+        arrange(cell, dates) %>%
         mutate(lagged = dplyr::lag(rainy_bin),
                streak_start = case_when(row_number() == 1 & !is.na(rainfall) ~ TRUE  # If cell's first date and there is rainfall data, then it's first day of a streak. If not, check if this day's value is the same as previous row's.
                                       ,row_number() == 1 & is.na(rainfall) ~ NA
@@ -62,27 +60,52 @@ stats <- lf %>%
         ungroup() %>%
         arrange(cell, dates)
 
-## identify season onset
+## identify season onset & cessation
+stats <- lf %>%
+  group_by(cell) %>%
+  arrange(cell, dates) %>%
+  dplyr::mutate(roll_sum_next_10d = ifelse(!is.na(rainfall), zoo::rollsum(rainfall, k = 10, fill = NA, align = 'left'), NA), # sum of precipitation in 10-day period starting today
+         start_10d_ds_bin = ifelse(roll_sum_next_10d < 2, 1, 0), # is this the first day of a dry 10-day period?
+         roll_sum_foll_15d = ifelse(!is.na(rainfall), zoo::rollsum(lead(rainfall, 1), k = 15, fill = NA, align = 'left'), NA), # sum of precipitation in 15-day period starting tomorrow (lead=1)
+         foll_by_15d_ds_bin = ifelse(roll_sum_foll_15d <= 25, 1, 0) # is tomorrow the first day of a 15-d dry period?
+  )
+
+# Onsets:
 # First day of a period after 1 Nov with at least 40mm of rain over 10 days 
 # AND no 10 consecutive days with less than 2mm of total rain in the following 30 days (DCCMS 2008).
-#stats$roll_sum_prev_10d <- ifelse(!is.na(stats$rainfall), zoo::rollsum(stats$rainfall, k = 10, fill = NA, align = 'right'), NA) # sum of precipitation in previous 10 days
-stats$roll_sum_foll_10d <- ifelse(!is.na(stats$rainfall), zoo::rollsum(stats$rainfall, k = 10, fill = NA, align = 'left'), NA) # sum of precipitation in following 10 days
-stats$foll_by_10d_ds_bin <- ifelse(stats$roll_sum_foll_10d < 2, 1, 0)
-  
 onsets <- stats %>%
              group_by(cell) %>%
              arrange(dates) %>%
-             mutate(followed_by_ds = ifelse(!is.na(rainfall), zoo::rollsum(foll_by_10d_ds_bin, k = 20, align = 'left') >= 1, NA)) %>% # boolean: if at least 1 day is followed by 10-day cum sum less than 2. 20th day last chance to be followed by 10 days within 30-day period
-             filter(dates >= '2021-11-01' & roll_sum_foll_10d >= 40 & followed_by_ds == FALSE) %>% # after 1 Nov, at least 40mm of rain over 10 days since 1 Nov and not followed by 10 days with 2mm or less of rain within 30 days
+             mutate(followed_by_ds = ifelse(!is.na(rainfall), zoo::rollsum(lead(start_10d_ds_bin, 1), k = 20, align = 'left') >= 1, NA)) %>% # boolean: is there at least one 10-day period with cum sum less than 2 in the next 20 days starting tomorrow (lead = 1)? k=20th day is the last chance to start a 10d-period within 30-day period
+             filter(dates >= '2021-11-01' & roll_sum_next_10d >= 40 & followed_by_ds == FALSE) %>%
              slice(which.min(dates)) %>% # retrieve earliest date that meets criterion per cell 
              ungroup() %>%
-             select(cell, date_chr, dates, roll_sum_foll_10d, followed_by_ds) %>%
+             select(cell, date_chr, dates, roll_sum_next_10d, followed_by_ds) %>%
              rename(onset_date_chr = date_chr, onset_date = dates) %>%
              mutate(onset_days_since_1nov = as.numeric(difftime(onset_date, as.Date("2021-11-01"), unit = "days"))) %>% # Day 0 = 1 Nov
              as.data.frame()
 
+# Cessations:
+# Last day before a 15-day period after 15 March with 25mm or less of rain
+cessations <- stats %>%
+            group_by(cell) %>%
+            arrange(dates) %>%
+            filter(dates >= '2022-03-15' & foll_by_15d_ds_bin == TRUE) %>% 
+            slice(which.min(dates)) %>% # retrieve earliest date that meets criterion per cell 
+            ungroup() %>%
+            select(cell, date_chr, dates, roll_sum_foll_15d, foll_by_15d_ds_bin) %>%
+            rename(cessation_date_chr = date_chr, cessation_date = dates) %>%
+            mutate(cessation_days_since_1nov = as.numeric(difftime(cessation_date, as.Date("2021-11-01"), unit = "days"))) %>% # Day 0 = 1 Nov
+            as.data.frame()
+
+# Duration:
+season_summary <- onsets %>%
+                    left_join(cessations, by = 'cell') %>%
+                    select(cell, onset_date, onset_days_since_1nov, cessation_date, cessation_days_since_1nov) %>%
+                    mutate(duration = as.numeric(cessation_date - onset_date))
+
 #####
-# create raster with results
+# create raster files with results
 #####
 
 # create blank raster and cell number list
