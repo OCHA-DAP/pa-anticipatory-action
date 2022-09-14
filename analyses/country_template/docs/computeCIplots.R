ebefeacd37fc1a993868688fc210076754bcea8d
Josee Poirier <josee.phd@gmail.com>
2022-09-14 15:01:33 -0400

    added 68% range labels

--- analyses/country_template/docs/computeCIplots.R
+++ analyses/country_template/docs/computeCIplots.R
@@ -8,7 +8,7 @@ plotCI <- function(trigger_id, metric_name) {
 
   # select performance metrics for this trigger_id and metric_name
   perf_metrics_sub <- perf_metrics_data %>%
-    filter(trigger == trigger_id & metric == metric_name))
+    filter(trigger == trigger_id & metric == metric_name)
 
   # compute segment low/high end values
   segment_dimensions[which(segment_dimensions$segment == 'seg_below_95'), 'hi_end'] <- perf_metrics_sub[which(perf_metrics_sub$upoint == 'low_end_95'), 'value']
@@ -21,13 +21,12 @@ plotCI <- function(trigger_id, metric_name) {
   segment_dimensions[which(segment_dimensions$segment == 'seg_above_95'), 'lo_end'] <- perf_metrics_sub[which(perf_metrics_sub$upoint == 'high_end_95'), 'value']
 
   # plot all segments
-  ci_color <- ifelse(metric %in% c('var', 'det'), "#1bb580", ifelse(metric %in% c('min', 'ful', 'atv'), "#007ce1" ,"#FF3333")) # select green for detection and valid activation rates, blue for framework probabilities, red for the others
+  ci_color <- ifelse(metric_name %in% c('var', 'det'), "#1bb580", ifelse(metric == 'atv', "#007ce1" ,"#FF3333")) # select green for detection and valid activation rates, blue for activation probs, red for error metrics
   ci_color_pale <- alpha(ci_color, 0.7)
 
   central.x <- perf_metrics_sub[which(perf_metrics_sub$upoint == 'central_95'), 'value'] # value of central value of 95% confidence interval
 
-  #p <-
-    ggplot(segment_dimensions, aes(xmin = lo_end, xmax = hi_end, ymin = 0, ymax = 1)) +
+  p <- ggplot(segment_dimensions, aes(xmin = lo_end, xmax = hi_end, ymin = 0, ymax = 1)) +
     geom_rect(aes(fill = segment), colour = NA) +
     scale_fill_manual(values=c('seg_below_95' = 'azure2',
                                'seg_95to68' = ci_color_pale,
@@ -45,64 +44,25 @@ plotCI <- function(trigger_id, metric_name) {
                color = "black",
                fill = "white",
                fontface = 'bold',
-               label.size = NA)  # removes border
-
-
-    geom_text(
-      aes(x=low_end, y=0, label=low_end),
-      size = 4, vjust = 0, hjust = 0, nudge_x = -2) +
-    geom_text(
-      aes(x=median_x, y=0.5, label=median_x),
-      size = 8, vjust = 0, hjust = 0, nudge_x = 0)
-
-
-
-
-# compute labels
-low_end_label <- ci_widths_df %>% filter(point == 'low_end') %>% select(value) %>% as.numeric()
-central_label <- ci_widths_df %>% filter(point == 'central') %>% select(value) %>% as.numeric()
-high_end_label <- ci_widths_df %>% filter(point == 'high_end') %>% select(value) %>% as.numeric()
-
-# plot
-ci_color <- ifelse(metric %in% c('var', 'det'), "#1bb580", ifelse(metric %in% c('min', 'ful', 'atv'), "#007ce1" ,"#FF3333")) # select green for detection and valid activation rates, blue for framework probabilities, red for the others
-
-metric_plot <- ci_df %>%
-  ggplot(aes(fill = point, y = value, x = dummy, labels = "low", "central", "high")) +
-  geom_bar(position = "stack", stat = "identity", alpha = 0.9, width = 1) +
-  scale_fill_manual(values = c("azure2", ci_color, "azure2")) +
-  scale_x_continuous(limits = c(0, 5)) +
-  coord_flip() +
-  geom_segment(y = central.x, yend = central.x, x = 0.5, xend = 1.6, color = "#444444", size = 0.75) + # central estimate line. Size refers to line thickness
-  geom_segment(y = low.x, yend = low.x, x = 0.5, xend = 1.6, color = "#444444", size = 0.5) + # low ci end line
-  geom_segment(y = high.x, yend = high.x, x = 0.5, xend = 1.6, color = "#444444", size = 0.5) + # high ci end line
-  geom_label(aes(x = 2,
-                 y = central.x,
-                 label = central_label,
-                 vjust = 1),
-             size = 8, # font size
-             nudge_x = 0.1, # bring label closer to graph
-             color = "black",
-             fill = "white",
-             fontface = 'bold',
-             label.size = NA) + # removes border
-  geom_label(aes(x = 2,
-                 y = low.x,
-                 label = low_end_label,
-                 vjust = 1),
-             size = 7,
-             nudge_x = 0,
-             color = "black",
-             fill = "white",
-             label.size = NA) + # removes border
-  geom_label(aes(x = 2,
-                 y = high.x,
-                 label = high_end_label,
-                 vjust = 1),
-             size = 7,
-             nudge_x = 0,
-             color = "black",
-             fill = "white",
-             label.size = NA) + # removes border
+               label.size = NA) + # removes border
+    geom_label(aes(x = segment_dimensions[which(segment_dimensions$segment == 'seg_68'), 'lo_end'],
+                     y = 1,
+                     label = segment_dimensions[which(segment_dimensions$segment == 'seg_68'), 'lo_end'],
+                     vjust = -0.1),
+                 size = 5,
+                 nudge_x = 0,
+                 color = "black",
+                 fill = "white",
+                 label.size = NA) +
+    geom_label(aes(x = segment_dimensions[which(segment_dimensions$segment == 'seg_68'), 'hi_end'],
+                     y = 1,
+                     label = segment_dimensions[which(segment_dimensions$segment == 'seg_68'), 'hi_end'],
+                     vjust = -0.1),
+                 size = 5,
+                 nudge_x = 0,
+                 color = "black",
+                 fill = "white",
+                 label.size = NA) +
   theme_economist_white(base_size = 11,
                         base_family = "sans",
                         gray_bg = F,
@@ -117,18 +77,21 @@ metric_plot <- ci_df %>%
     panel.grid.major = element_blank(),
     panel.grid.minor = element_blank())
 
+  return(list(plot = p, segment_dims = segment_dimensions))
+}
+
 # Save original as png
 
-trigger_label <- ifelse(trigger_id == 'framework', 'framework', paste0('trigger_', trigger_id))
-
-filename <- paste0(trigger_label, "_", metric, "_ci.png")
-png(filename = paste0("plots/", filename), width = 815, height = 410, units = "px")
-print(metric_plot)
-dev.off()
-
-# crop plot
-metric_magick <- magick::image_read(paste0("plots/", filename))
-trimmed_metric <- magick::image_trim(metric_magick)
-magick::image_write(trimmed_metric, path = paste0("plots/trimmed_", trigger_label, "_", metric, "_ci.png"), format = "png")
-
-}
+#trigger_label <- ifelse(trigger_id == 'framework', 'framework', paste0('trigger_', trigger_id))
+#
+# filename <- paste0(trigger_label, "_", metric, "_ci.png")
+# png(filename = paste0("plots/", filename), width = 815, height = 410, units = "px")
+# print(metric_plot)
+# dev.off()
+#
+# # crop plot
+# metric_magick <- magick::image_read(paste0("plots/", filename))
+# trimmed_metric <- magick::image_trim(metric_magick)
+# magick::image_write(trimmed_metric, path = paste0("plots/trimmed_", trigger_label, "_", metric, "_ci.png"), format = "png")
+#
+# }
