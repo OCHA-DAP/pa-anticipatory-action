5a4c51d752582d4bbad196b47bcf27e3897cf7f4
turnerm <turnermon@gmail.com>
2022-10-27 14:05:31 +0200

    Update to new file

--- src/indicators/flooding/glofas/get_stations.py
+++ src/indicators/flooding/glofas/get_stations.py
@@ -10,11 +10,10 @@ import pandas as pd
 
 DATA_DIR = Path(os.environ["AA_DATA_DIR"])
 FILENAME = (
-    DATA_DIR
-    / "private/exploration/glb/glofas/Qgis_World_outlet_202104_20210421.csv"
+    DATA_DIR / "private/exploration/glb/glofas/Qgis_World_outlet_20221004.csv"
 )
 
-country_code = sys.argv[1]
+country_code = sys.argv[1].upper()
 
 df = pd.read_csv(
     FILENAME,
@@ -27,12 +26,24 @@ df = pd.read_csv(
         "LisfloodY",
     ],
 )
-df = df.loc[df["Country code"] == country_code].sort_values(by="station_id")
+df = df.loc[df["Country code"] == country_code].sort_values(
+    by=["StationName", "station_id"]
+)
+
+# Add suffix
+g = df.groupby("StationName")
+df["StationName"] += (
+    g.cumcount()
+    .add(1)
+    .astype(str)
+    .radd(" ")
+    .mask(g["StationName"].transform("count") == 1, "")
+)
 
 print("glofas:")
 print("  reporting_points:")
 for _, row in df.iterrows():
-    print(f"  - id: {row['station_id']}")
-    print(f"    name: {row['StationName']}")
+    # print(f"  - id: {row['station_id']}")
+    print(f"  - name: {row['StationName']}")
     print(f"    lon: {row['LisfloodX']}")
     print(f"    lat: {row['LisfloodY']}")
